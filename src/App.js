b83477c241feb0203d0618c214c38c5df2206cc4
Bhagyadilhara <bhagyadilhara123@gmail.com>
2024-01-15 10:30:20 +0530

    add states

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState({
+    title: "",
+    date: "",
+    status: "",
+    priority: "",
+  });
+
+  const handleOnChange = (e) =>{
+    //console.log(e.target.value);
+    setTasks({...tasks,[e.target.name]: e.target.value});
+  }
+
+
+
   return (
     <div className="App">
-     Bhagya 
+      <div className="h-screen w-auto bg-blue-300">
+        <div className='flex items-center justify-between'>
+          <div className='flex flex-col'>
+            <label>Task</label>
+            <input 
+              name='title' 
+              onChange={(e)=> handleOnChange(e)}
+              value={tasks.title}
+              placeholder='task'
+            />
+          </div>
+          
+        </div>
+      </div>
     </div>
   );
 }
